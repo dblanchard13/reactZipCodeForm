ee123687e355ba1bc1108aaf3013b45ef86e3965
dblanchard13 <davidb.prof@gmail.com>
2016-08-29 22:14:32 -0700

    Move webpack to normal dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -22,21 +22,10 @@ module.exports = {
   },
 
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      }
-    ],
     loaders: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
       }
     ]
   }
